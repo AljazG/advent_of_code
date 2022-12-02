968a1499fac4314005784142db990962eb33cecb
aljazg <aljazgrubar@gmail.com>
2022-12-02 16:08:50 +0100

    day 2 alternative solution

---
+++ day02/firstalt.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+
+using namespace std;
+
+map<string, int> rpsMap = {
+    {"AX", 4},
+    {"AY", 8},
+    {"AZ", 3},
+    {"BX", 1},
+    {"BY", 5},
+    {"BZ", 9},
+    {"CX", 7},
+    {"CY", 2},
+    {"CZ", 6},
+};
+
+int main() {
+
+    ifstream file;
+
+    file.open("../data/day02/input.txt");
+
+    long sum = 0;
+
+    if (file.is_open()){
+        string line;
+        while (getline(file, line)) {
+            int score = rpsMap[string(1, line[0]) + line[2]];
+            sum += score;
+        }
+    }
+
+    file.close();
+
+    cout << "sum: " << sum;
+
+    return 0;
+}
