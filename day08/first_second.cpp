f92b63ec2a9b9649d9e37337ef775faadb8205e7
aljazg <aljazgrubar@gmail.com>
2022-12-08 20:59:28 +0100

    day 8

--- day08/first_second.cpp
+++ day08/first_second.cpp
@@ -53,7 +53,6 @@ int getScenicScore( int i, int j, int** grid, int cols, int rows) {
     int up = 0;
     int down = 0;
 
-
     //check left
     for (int x = j-1; x >= 0; x--) {
         left++;
@@ -89,8 +88,6 @@ int getScenicScore( int i, int j, int** grid, int cols, int rows) {
     return right * left * up * down;
 }
 
-
-
 int main() {
 
     vector<string> trees = input_utils::readLines("../data/day08/input.txt");
@@ -110,8 +107,6 @@ int main() {
 
     int counter = 0;
 
-
-
     for (int i = 1; i < rows-1; i++) {
         for (int j = 1; j < cols - 1; j++) {
             if (isVisible(i, j, grid, cols, rows)) {
@@ -119,11 +114,11 @@ int main() {
             }
         }
     }
+
     int scenicScoreMax = 0;
 
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
-
             int score = getScenicScore(i, j, grid, cols, rows);
             if (score > scenicScoreMax) {
                 scenicScoreMax = score;
@@ -138,7 +133,3 @@ int main() {
 
     return 0;
 }
-
-
-
-
